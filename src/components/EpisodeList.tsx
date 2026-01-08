ddce0de920899a47e80ca321beb2419fdca369c7
Jim Barnes <jimbarnesdeveloper@gmail.com>
2026-01-08 11:18:11 -0500

    Added simple pagination

--- src/components/EpisodeList.tsx
+++ src/components/EpisodeList.tsx
@@ -16,14 +16,23 @@ function EpisodeList(props: EpisodeListProps) {
   const [episodes, setEpisodes] = useState<Array<PodcastEpisode>>();
   const searchServiceURL = import.meta.env.VITE_SEARCH_SERVICE_URL;
 
+  const [limit] = useState(props.limit);
+  const [page, setPage] = useState(props.page || 0);
+  const [pageCount, setPageCount] = useState(0);
+
   useEffect(() => {
     const baseUrl = `${searchServiceURL}/api/v1/podcasts/episodes/`;
     const params: Record<string, string> = {};
+    const _limit = limit || 10;
+    const _page = page || 0;
 
     if (props.showId) params['show'] = props.showId.toString();
     if (props.category) params['category'] = props.category;
     if (props.search) params['search'] = props.search;
     if (props.tags) params['tags'] = props.tags.join(',');
+
+    params['limit'] = _limit.toString();
+    params['offset'] = (_limit * _page).toString();
 
     params['fields'] = 'id';
 
@@ -36,22 +45,56 @@ function EpisodeList(props: EpisodeListProps) {
       }
 
       const result = await response.json();
+
+      // Get Page count
+      const pageCount = Math.ceil(result.count / _limit);
+      setPageCount(pageCount);
       setEpisodes(result.results);
     };
 
     fetchData();
-  }, [props, searchServiceURL])
+  }, [props, searchServiceURL, limit, page])
+
+  const setCurrentPage = (delta: number) => {
+    const _page = page || 0;
+    let newPageIndex = _page + delta;
+
+    if (newPageIndex < 0) newPageIndex = 0;
+    if (newPageIndex > pageCount! - 1) newPageIndex = pageCount - 1;
+
+    setPage(newPageIndex);
+  };
 
   return (
-    <div className='row'>
-      {episodes && ( episodes.map((episode) => {
-        return (
-          <div key={episode.id} className='col-md-6'>
-            <EpisodeCard episodeId={episode.id} />
-          </div>
-        )
-      }))}
-    </div>
+    <>
+      <div className='row'>
+        {episodes && ( episodes.map((episode) => {
+          return (
+            <div key={episode.id} className='col-md-6'>
+              <EpisodeCard episodeId={episode.id} />
+            </div>
+          )
+        }))}
+      </div>
+      { pageCount > 1 && (
+        <nav aria-label="Result pagination">
+          <ul className='pagination'>
+            <li className='page-item'><a className='page-link' href='#' onClick={() => setCurrentPage(-1)}>Previous</a></li>
+            {[...Array(pageCount)].map((_, index) => (
+              <li key={index} className={`page-item ${index == page ? 'active' : ''}`}>
+                <a
+                  className='page-link'
+                  href='#'
+                  onClick={() => setPage(index)}>
+                    {index + 1}
+                </a>
+              </li>
+            ))}
+            <li className='page-item'><a className='page-link' href='#' onClick={() => setCurrentPage(1)}>Next</a></li>
+          </ul>
+        </nav>
+      )}
+    </>
   );
 }
 
